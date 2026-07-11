0955e688feca84c1fe0695d426c4e8a1111be232
Brian <mulindijrn@gmail.com>
2026-07-11 18:17:07 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-blue-600">
+        WeatherAI Smart Planner
+      </h1>
+    </div>
+  );
+};
+
+export default App;
